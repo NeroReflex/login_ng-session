45fbaddfead8f7f66ced64cc047650abff13cce9
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-07-11 11:54:53 +0200

    Fix make_release.sh script

--- make_release.sh
+++ make_release.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+
+set -e
+
 new_version=${1}
-version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' login_ng-session/Cargo.toml)
+version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' login_ng-session/Cargo.toml | head -n 1)
+echo "Replacing version $version with $new_version"
 find . -name "*.toml"  -type f -exec sed -i "s/\"$version\"/\"$new_version\"/g" {} +
-#sed -i "s/\"$version\"/\"$new_version\"/g" Cargo.lock
-cargo build
+cargo bitbake
 git add .
 git commit -m "Bump version to $new_version"
 git tag $new_version
